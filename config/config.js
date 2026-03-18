261a1bb9bd56ffc9239f0bb7dabf59a6a6c8e2d8
Enzo Vasconcelos <enzo.vasconcelos@ccc.ufcg.edu.br>
2026-03-17 23:38:41 -0300

    kj

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
-const url = "http://localhost:3000/api";
-// const url = 'https://web2-back-production.up.railway.app/api';
+// const url = "http://localhost:3000/api";
+const url = 'https://web2-back-production.up.railway.app/api';
 
 export const getBaseServerUrl = () => {
     return url;
